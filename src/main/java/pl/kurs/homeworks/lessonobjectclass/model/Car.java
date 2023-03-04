85c3af8261730dae16d30bb1f5cd5f70f3263480
“[Wiktor <[wiktorszczepanik126p@gmail.com]>
2023-03-04 14:45:30 +0100

    added Car class

---
+++ src/main/java/pl/kurs/homeworks/lessonobjectclass/model/Car.java
@@ -0,0 +1,57 @@
+package pl.kurs.homeworks.lessonobjectclass.model;
+
+import java.util.Objects;
+
+public class Car {
+    private String producer;
+    private String model;
+
+    public Car(String producer, String model) {
+        this.producer = producer;
+        this.model = model;
+    }
+
+    public Car(Car carForCopy) {// Konstruktor klonujący.
+        this.producer = carForCopy.producer;
+        this.model = carForCopy.model;
+    }
+
+
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(producer, car.producer) && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(producer, model);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "producer='" + producer + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+}
